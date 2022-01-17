1f51d5e9131963d6d0424b6e3a1749e3bd95398c
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2022-01-16 20:17:55 -0600

    testEncryptionAdded

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -16,10 +17,16 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
   useNewUrlParser: true
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ["password"]
+});
 
 const User = new mongoose.model("User", userSchema);
 
